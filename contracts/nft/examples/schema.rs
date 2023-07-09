761d8f28d92c435d4273a6b23ec9c9ee093748a4
wwrdumbaf <ktwestkween@gmail.com>
2023-07-09 11:44:44 -0700

    gfdi

--- contracts/nft/examples/schema.rs
+++ contracts/nft/examples/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw721_metadata_onchain::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use nft::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
